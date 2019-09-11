9c1b145f07425c81f1ca057d37b6c0e065ac9e4a
diegopmoreira <diegopm07@gmail.com>
2019-09-11 02:04:45 -0300

    fixed type test and setting up circle ci

--- test/get/type_get.js
+++ test/get/type_get.js
@@ -7,46 +7,27 @@ const _ = require("lodash");
 describe("Pokedex tests", () => {
   before(done => {
     request(url)
-      .get("/pokemon/")
+      .get("/type/")
       .end((err, res) => {
-        pokemon = _.sample(res.body.results);
-        pokemon.id = pokemon.url.split(url)[1].split("/")[0];
+        type = _.sample(res.body.results);
         done(err);
       });
   });
 
-  describe("GET /pokemon/{id or name}", () => {
+  describe("GET /type/{id or name}", () => {
     it("name must be valid", done => {
       request(url)
-        .get(`/pokemon/${pokemon.name}/`)
+        .get(`/type/${type.name}/`)
         .end((err, res) => {
           assert.equal(res.status, 200);
-          assert.equal(res.body.name, pokemon.name);
-          assert.equal(res.body.id, pokemon.id);
+          assert.equal(res.body.name, type.name);
           done(err);
         });
     });
 
     it('with invalid "name" ', done => {
       request(url)
-        .get("/pokemon/agumon/")
-        .expect(404, done);
-    });
-
-    it("id must be valid", done => {
-      request(url)
-        .get(`/pokemon/${pokemon.id}/`)
-        .end((err, res) => {
-          assert.equal(res.status, 200);
-          assert.equal(res.body.name, pokemon.name);
-          assert.equal(res.body.id, pokemon.id);
-          done(err);
-        });
-    });
-
-    it('with invalid "id" ', done => {
-      request(url)
-        .get("/pokemon/96669/")
+        .get("/type/fogo/")
         .expect(404, done);
     });
   });
