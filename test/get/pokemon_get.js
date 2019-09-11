1f7dee1909249bd56fec0c88b776e644a530686f
diegopmoreira <diegopm07@gmail.com>
2019-09-11 01:45:44 -0300

    added get tests

---
+++ test/get/pokemon_get.js
@@ -0,0 +1,55 @@
+const request = require("supertest");
+const assert = require("chai").assert;
+
+const url = "https://pokeapi.co/api/v2";
+const _ = require("lodash");
+
+describe("PokeApi Tests", () => {
+  before(done => {
+    request(url)
+      .get("/pokemon/")
+      .end((err, res) => {
+        pokemon = _.sample(res.body.results);
+        pokemon.id = pokemon.url
+          .split("https://pokeapi.co/api/v2/pokemon/")[1]
+          .split("/")[0];
+        done(err);
+      });
+  });
+
+  describe("GET /pokemon/{id or name}", () => {
+    it("name must be valid", done => {
+      request(url)
+        .get(`/pokemon/${pokemon.name}/`)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.name, pokemon.name);
+          assert.equal(res.body.id, pokemon.id);
+          done(err);
+        });
+    });
+
+    it('with invalid "name" ', done => {
+      request(url)
+        .get("/pokemon/agumon/")
+        .expect(404, done);
+    });
+
+    it("id must be valid", done => {
+      request(url)
+        .get(`/pokemon/${pokemon.id}/`)
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.name, pokemon.name);
+          assert.equal(res.body.id, pokemon.id);
+          done(err);
+        });
+    });
+
+    it('with invalid "id" ', done => {
+      request(url)
+        .get("/pokemon/96669/")
+        .expect(404, done);
+    });
+  });
+});
